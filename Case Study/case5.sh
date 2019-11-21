fbd217a54da368501271a384a92d4fbefc936f3f
AjayKumarTyagi <46739062+AjayKumarTyagi@users.noreply.github.com>
2019-11-21 13:10:34 +0530

    updated case5

--- Case Study/case5.sh
+++ Case Study/case5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "enter port: "
 read port
-c=$(netstat -anp | grep ":$port")
-if [ "x$c" = "x" ]
+c=$(netstat -anp | grep -o ":$port")
+if [ -z $c ]
 then
  /etc/init.d/apache2 restart
  echo "server restarted"
@@ -10,7 +10,7 @@ else
  echo "server already running"
 fi
 path=$(realpath "$0")
-if ! (crontab -l | grep -Fxq "*/1 * * * * $path");
+if ! (crontab -l | grep -Fq "*/1 * * * * $path");
 then
  crontab -l | { cat; echo "*/1 * * * * $path"; } | crontab
  echo "script added"
